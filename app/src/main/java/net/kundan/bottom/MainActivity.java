a034cfaf6195ed94c47a635fb450f3e12a2f4ad3
Kundan Kumar <48217739+Kundan9122@users.noreply.github.com>
2020-07-26 11:36:26 +0530

    Navigation

---
+++ app/src/main/java/net/kundan/bottom/MainActivity.java
@@ -0,0 +1,92 @@
+package net.simplifiedcoding.bottom;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.MenuItem;
+
+//implement the interface OnNavigationItemSelectedListener in your activity class
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        final BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+        underlineMenuItem(bottomNavigationView.getMenu().getItem(0)); // underline the default selected item when the activity is launched
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        removeItemsUnderline(bottomNavigationView); // remove underline from all items
+                        underlineMenuItem(item); // underline selected item
+                        switch (item.getItemId()) {
+                            // handle item clicks
+                        }
+                        return false;
+                    }
+                });
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new DashboardFragment();
+                break;
+
+
+
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    private void removeItemsUnderline(BottomNavigationView bottomNavigationView) {
+        for (int i = 0; i <  bottomNavigationView.getMenu().size(); i++) {
+            MenuItem item = bottomNavigationView.getMenu().getItem(i);
+            item.setTitle(item.getTitle().toString());
+        }
+    }
+
+    private void underlineMenuItem(MenuItem item) {
+        SpannableString content = new SpannableString(item.getTitle());
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        item.setTitle(content);
+}}
